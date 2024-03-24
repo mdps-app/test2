32f523c3ab91aaf579585fbb4ac0cd785df3a392
shiki <kishintorii@gmail.com>
2024-03-24 17:57:07 +0900

    📁 save
    ヘッダー作成中

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,160 @@
 	import { page } from '$app/stores';
 </script>
 
-{#if !$page.data.user}
-	<a href="/login">ログイン</a>
-	<a href="/register">新規登録</a>
-{:else}
-	<form action="/logout?/logout" method="POST">
-		<button>ログアウト</button>
-	</form>
-{/if}
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+</svelte:head>
 
-<slot />
+<header>
+	<div>
+		<p>備品管理システム</p>
+	</div>
+	<nav>
+		<ul>
+			<li>
+				<p>ダッシュボード</p>
+				<span class="material-symbols-outlined"> dashboard </span>
+			</li>
+			<li>
+				<p>一覧</p>
+				<span class="material-symbols-outlined"> database </span>
+			</li>
+			<li>
+				<p>配置図</p>
+				<span class="material-symbols-outlined"> map </span>
+			</li>
+			<li>
+				<p>設定</p>
+				<span class="material-symbols-outlined"> settings </span>
+			</li>
+			<li id="menu">
+				<div>
+					{#if !$page.data.user}
+						<a href="/login">ログイン</a>
+						<p>/</p>
+						<a href="/register">新規登録</a>
+					{:else}
+						<form action="/logout?/logout" method="POST">
+							<button>ログアウト</button>
+						</form>
+					{/if}
+				</div>
+				<span class="material-symbols-outlined"> menu </span>
+			</li>
+		</ul>
+	</nav>
+</header>
+<main>
+	<slot />
+</main>
+
+<style lang="scss">
+	header {
+		z-index: 999;
+		width: 300px;
+		height: 100vh;
+		background-color: rgb(115, 192, 255);
+		position: fixed;
+
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 80px 1fr;
+		gap: 20px;
+
+		nav {
+			height: 100%;
+		}
+
+		ul {
+			width: 100%;
+			height: 100%;
+
+			margin: 0;
+			padding: 0;
+
+			display: grid;
+			grid-template-columns: 1fr;
+			grid-template-rows: repeat(4, 100px) 1fr 100px;
+			gap: 10px;
+
+			li {
+				list-style: none;
+				cursor: pointer;
+
+				display: grid;
+				grid-template-columns: 1fr 80px;
+
+				p {
+					font-size: 1.5em;
+					display: flex;
+					justify-content: right;
+					align-items: center;
+				}
+				span {
+					font-size: 2.5em;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				}
+			}
+
+			#menu {
+				grid-row: 6 / 7;
+				cursor: default;
+
+				div {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+
+					a {
+						float: left;
+						cursor: pointer;
+
+						font-size: 1.1em;
+						color: rgb(22, 100, 201);
+						text-decoration: none;
+
+						transition: color 0.3s;
+
+						&:hover {
+							color: rgb(247, 251, 255);
+						}
+					}
+					p {
+						float: left;
+						margin-left: 10px;
+						margin-right: 10px;
+
+						color: rgb(22, 100, 201);
+					}
+				}
+
+				span {
+					grid-column: 2 / 3;
+
+					cursor: pointer;
+				}
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
-    import {goto} from "$app/navigation";
-    import type { PageData } from "./$types";
-    import Thread from "$lib/Thread.svelte";
-    export let data: PageData
+	import { goto } from '$app/navigation';
+	import type { PageData } from './$types';
+	import Thread from '$lib/Thread.svelte';
+	export let data: PageData;
 </script>
+
+<svelte:head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
+	<title>Document</title>
+</svelte:head>
+
 <main>
-   <h1>スレッド一覧</h1>
-    <button type="button"  on:click={() => goto("/post")}>投稿する</button>
-    {#each data.threads as thread }
-       <Thread id={thread.id} content={thread.content} />
-    {/each}
+	<h1>スレッド一覧</h1>
+	<button type="button" on:click={() => goto('/post')}>投稿する</button>
+	{#each data.threads as thread}
+		<Thread id={thread.id} content={thread.content} />
+	{/each}
 </main>
+
+<style lang="scss">
+    
+</style>
